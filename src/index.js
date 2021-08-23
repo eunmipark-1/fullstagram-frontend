e51555b96eb148ac335db94aed1f61202a20e926
mia park <mia1221@kakao.com>
2021-08-23 13:26:33 +0900

    Route + Apollo Client + mini style

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { ApolloProvider } from 'react-apollo-hooks';
 import ReactDOM from 'react-dom';
+import Client from './Apollo/Client';
 
 import App from './Components/App';
 
-
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<ApolloProvider client={Client}>
+			<App />
+		</ApolloProvider>
+	</React.StrictMode>,
+	document.getElementById('root'),
 );
