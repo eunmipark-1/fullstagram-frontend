4073c8943c05800e1095cb4e616377f923b3f82a
mia park <mia1221@kakao.com>
2021-08-23 16:56:54 +0900

    auth input

---
+++ src/Components/Input.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Container = styled.input`
+	border:0;
+	border:${(props) => props.theme.boxBorder};
+	border-radius: ${(props) => props.theme.borderRadius}
+	background-color: ${(props) => props.theme.bgColor};
+	height:35px;
+	font-size:12px;
+	padding:0 15px;
+`;
+
+const Input = ({ placeholder, required = true, onChange, value, type = 'text', className }) => (
+	<Container
+		placeholder={placeholder}
+		required={required}
+		value={value}
+		onChange={onChange}
+		type={type}
+		className={className}
+	/>
+);
+
+Input.propTypes = {
+	placeholder: PropTypes.string.isRequired,
+	required: PropTypes.bool,
+	value: PropTypes.string.isRequired,
+	onChange: PropTypes.func.isRequired,
+	type: PropTypes.string,
+};
+
+export default Input;
--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -3,15 +3,22 @@ import reset from 'styled-reset';
 
 export default createGlobalStyle`
 	${reset};
+	@import url('https://fonts.googleapis.com/css?family=Open+Sans:400,500,700');
 	* {
 		box-sizing:border-box;
 	}
 	body {
 		background-color: ${(props) => props.theme.bgColor}
 		color: ${(props) => props.theme.blackColor}
+		font-size:14px;
+		font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ununtu, Cantarell, 'Open Sans', "Helvetica Nenu', sans-serif;
+		padding-top:140px;
 	}
 	a {
 		color: ${(props) => props.theme.blueColor}; 
 		text-decoration:none;
+	}
+	input:focus{
+		outline:none;
 	}
 `;
