3e9c3d6bde453588483cbe10d9d232a92fef1fc3
mia park <mia1221@kakao.com>
2021-08-23 17:58:27 +0900

    auth container + presenter 로 분리

--- src/Components/Input.js
+++ src/Components/Input.js
@@ -3,13 +3,13 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const Container = styled.input`
-	border:0;
-	border:${(props) => props.theme.boxBorder};
-	border-radius: ${(props) => props.theme.borderRadius}
+	border: 0;
+	border: ${(props) => props.theme.boxBorder};
+	border-radius: ${(props) => props.theme.borderRadius};
 	background-color: ${(props) => props.theme.bgColor};
-	height:35px;
-	font-size:12px;
-	padding:0 15px;
+	height: 35px;
+	font-size: 12px;
+	padding: 0 15px;
 `;
 
 const Input = ({ placeholder, required = true, onChange, value, type = 'text', className }) => (
--- src/Styles/GlobalStyles.js
+++ src/Styles/GlobalStyles.js
@@ -8,8 +8,8 @@ export default createGlobalStyle`
 		box-sizing:border-box;
 	}
 	body {
-		background-color: ${(props) => props.theme.bgColor}
-		color: ${(props) => props.theme.blackColor}
+		background-color: ${(props) => props.theme.bgColor};
+		color: ${(props) => props.theme.blackColor};
 		font-size:14px;
 		font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ununtu, Cantarell, 'Open Sans', "Helvetica Nenu', sans-serif;
 		padding-top:140px;
